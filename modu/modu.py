2d91ea57a8f6ad33d617ea93cbd93a563038bbae
huangdayu <1161946342@qq.com>
2018-03-12 00:05:04 +0800

    黄大宇

--- modu/modu.py
+++ modu/modu.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-import app.wakeup_trigger_main as wakeup_trigger_main
+from app import wakeup_trigger_main
 
 while True:
-    if wakeup_trigger_main.getText() != "":
+    if "" != wakeup_trigger_main.getText():
         print(wakeup_trigger_main.getText())
     else:
         pass
