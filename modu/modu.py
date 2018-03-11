a0547c974802b6fa398b0dba62798364340bd756
huangdayu <1161946342@qq.com>
2018-03-11 22:46:01 +0800

    黄大宇

--- modu/modu.py
+++ modu/modu.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 import wakeup_trigger_main
 
-def getText():
-    text=wakeup_trigger_main.getText()
-    text=re.sub('\'','\"',text)
-    textDict = json.loads(text) #产生dict
-    text=textDict['payload']['text']
-    print(text)
-    return text
+while True:
+    if wakeup_trigger_main.getText() != "":
+        print(wakeup_trigger_main.getText())
+    else:
+        pass
