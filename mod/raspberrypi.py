d79a01cda5485a3b1fc126c23cf791a36b20c8a9
huangdayu <1161946342@qq.com>
2018-03-24 23:21:28 +0800

    黄大宇

--- mod/raspberrypi.py
+++ mod/raspberrypi.py
@@ -91,7 +91,7 @@ DISK_used = DISK_stats[1] # print('DISK 使用 = '+str(DISK_used)+'B')
 DISK_perc = DISK_stats[3]# print('DISK 使用占百分比 = '+str(DISK_perc))
 
 def getRpiData():
-    data="树莓派IP地址是"+Rpi_IP()+"CPU温度"+getCPUtemperature()+"CPU使用率"+getCPUuse()+'RAM 总计= '+str(RAM_total)+' MB'+'RAM 使用 = '+str(RAM_used)+' MB'+'RAM 剩余 = '+str(RAM_free)+' MB'+'DISK 总计 = '+str(DISK_total)+'B'+'DISK 使用 = '+str(DISK_used)+'B'+'DISK 使用占百分比 = '+str(DISK_perc)
+    data="树莓派IP地址是"+Rpi_IP()+"CPU温度"+getCPUtemperature()+"CPU使用率"+getCPUuse()+'RAM 总计= '+RAM_total+' MB'+'RAM 使用 = '+RAM_used+' MB'+'RAM 剩余 = '+RAM_free+' MB'+'DISK 总计 = '+DISK_total+'B'+'DISK 使用 = '+DISK_used+'B'+'DISK 使用占百分比 = '+DISK_perc
     return data
 
 if __name__ == '__main__':
