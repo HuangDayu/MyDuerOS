eb6d902ed745fe24c949188bc20e702b1cee52db
huangdayu <1161946342@qq.com>
2018-04-01 16:08:55 +0800

    黄大宇

--- install.sh
+++ install.sh
@@ -6,8 +6,22 @@ sudo apt install gstreamer1.0-plugins-ugly
 sudo apt install python-gi
 sudo apt install gir1.2-gstreamer-1.0
 sudo apt install python-gst0.1
+sudo apt install gir1.2-gtk-3.0
+sudo apt install gstreamer1.0
+sudo apt install gstreamer1.0-plugins-good
+sudo apt install gstreamer1.0-plugins-ugly
+sudo apt install python-gi
+sudo apt install python-gst-1.0
+sudo apt install gir1.2-gstreamer-1.0
 sudo chmod 777 wakeup_trigger_start.sh auth.sh enter_trigger_start.sh
-sudo apt-get install -y python-dateutil gir1.2-gstreamer-1.0 python-pyaudio libatlas-base-dev python-dev python-configparser openssl mplayer
+sudo apt-get install -y python-dateutil
+sudo apt install gir1.2-gstreamer-1.0
+sudo apt install python-pyaudio
+sudo apt install libatlas-base-dev
+sudo apt install python-dev
+sudo apt install python-configparser
+sudo apt install openssl
+sudo apt install mplayer
 sudo pip install gi
 sudo pip install tornado
 sudo pip install hyper
