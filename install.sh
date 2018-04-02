f87701bb929a842babc97a547f5175e00364d4f3
huangdayu <1161946342@qq.com>
2018-04-03 00:15:57 +0800

    黄大宇

--- install.sh
+++ install.sh
@@ -1,21 +1,15 @@
 echo 'Install DuerOS-Modularization Python Dependency Library Now!'
 sudo chmod 777 wakeup_trigger_start.sh auth.sh enter_trigger_start.sh
 sudo apt-get update
-sudo apt install -y gstreamer1.0
-sudo apt install -y gstreamer1.0-plugins-good
-sudo apt install -y gstreamer1.0-plugins-ugly
-sudo apt install -y python-gi
-sudo apt install -y gir1.2-gstreamer-1.0
-sudo apt install -y gir1.2-gtk-3.0
-sudo apt install -y python-gst-1.0
-sudo apt install -y python-dateutil
-sudo apt install -y python-pyaudio
-sudo apt install -y libatlas-base-dev
-sudo apt install -y python-dev
+sudo apt install -y python-requests
 sudo apt install -y python-configparser
+sudo apt install -y python-dateutil
 sudo apt install -y openssl
+sudo apt install python-gi
+sudo apt install python-dateutil gir1.2-gstreamer-1.0 python-pyaudio libatlas-base-dev python-dev
+sudo apt install -y gstreamer1.0
 sudo apt install -y mplayer
-sudo pip install gi
+sudo pip uninstall gi
 sudo pip install tornado
 sudo pip install hyper
 sudo python2 ./aip/setup.py install
