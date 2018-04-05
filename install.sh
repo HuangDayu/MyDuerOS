62b6e713cf5c7878ecdfe38435dd81954ac7296b
huangdayu <1161946342@qq.com>
2018-04-05 14:50:34 +0800

    黄大宇

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ sudo chmod 777 wakeup_trigger_start.sh auth.sh enter_trigger_start.sh
 sudo apt-get update
 sudo pip uninstall gi
 sudo pip install tornado hyper
-sudo apt-get install -y python-dateutil gir1.2-gstreamer-1.0 python-pyaudio libatlas-base-dev python-dev
+sudo apt install -y python-dateutil gir1.2-gstreamer-1.0 python-pyaudio libatlas-base-dev python-dev
 sudo apt install -y python-requests python-gi python-configparser gstreamer1.0 openssl
 sudo python2 ./aip/setup.py install
